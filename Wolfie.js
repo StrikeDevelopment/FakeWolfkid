2f8da876f18d67c496a93378c3a0085785bbc9ae
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-05-29 19:41:41 -0500

    Update Wolfie.js

--- Wolfie.js
+++ Wolfie.js
@@ -4,7 +4,7 @@ const config = require('./config.json');
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('Neko Lewds!!!', {type: 'WATCHING'});
+    client.user.setActivity('Prefix - | -help', {type: 'WATCHING'});
     console.log('BOI I\'m ready!');
 });
 
@@ -14,93 +14,105 @@ client.on('message', async message => {
 
     if (message.content === prefix + 'help') {
       let helpembed = new Discord.RichEmbed()
-      .setTitle('Simple Comamnds | prefix ! ')
-      .setDescription('There is available commands for this bot on list!')
-      .addField('help', 'Bot commands list!')
-      .addField('media', 'Check The Bot Creator Website/Media')
-      .addField('dcinvite', 'Join our developer team on Discord server!')
-      .addField('info', 'Info about the Guild')
-      .addField('avatar', 'See your profile picture But Bigger')
-      .addField('changelog', 'View the New Updates that have bene made to the Bot!')
-      .addField('botinfo', 'VIEW Bof Information and Versions')
+      .setTitle('Simple Bot Comamnds | prefix - ')
+      .setDescription('There is available commands for this bot on list')
+      .addField('-help', 'Sends a list of bot commands')
+      .addField('-ban', 'Ban Command Beta')
+      .addField('-media', 'Links to our Social Media')
+      .addField('-info', 'information you may need to know about the bot')
+      .addField('-profile', 'New command > Check out that profile')
+      .addField('-changelog', 'changes made to bot updates every 5 minutes')
+      .addField('-donate', 'Donate to help pay bot VPS')
+      .addField('-announce', 'A Premium Feature')
       .setColor('RANDOM')
-      .setFooter(`Requested by ${message.author.tag} | Help page 1`)
+      .setFooter('Bot made by ZarkyMC-YT#7314 | Help Page 1')
       return message.channel.send(helpembed);
     }
 
-    if (message.content === prefix + 'info') {
+    if (message.content === prefix + 'ban') {
       let aboutembed = new Discord.RichEmbed()
-      .setFooter(`Do it JUST do IT ${message.author.tag}`)
-
-      .setTitle('Want More Info About the Server?')
-      .setDescription('Ok. Im a High Tech Bot That Can do a lot of Stuff Example tell you How many Members are in your Server or in Other Server My Code is a open source you can fine on GutHub i can do more but is time to do other stuff.')
-      .setColor('#FFFF00')
-      .addField('GitHub', 'https://github.com/FutureDeveloperZ/FakeWolfkid')
-      .addField('Report Issues', 'https://github.com/FutureDeveloperZ/FakeWolfkid/issues')
-      .setDescription('Join the Discord', 'http://bit.ly/FutureDevs')
+      .setTitle('Ban')
+      .setDescription('Shows A List of information on how to use the ban command!')
+      .addField('Cooldown', '3 Seconds')
+      .addField('Usage', '-ban [user] [time] [reason]')
+      .addField('Add-Ons', 'With premium you can add a time to the ban')
+      .setColor('RANDOM')
+      .setFooter('Ban Command Currently in Beta | ZarkysMC-YT#7314')
       return message.channel.send(aboutembed);
     }
 
     if (message.content === prefix + 'media') {
       let mediaembed = new Discord.RichEmbed()
-      .setTitle('This is Wolfie Social Media')
-      .addField('YouTube', 'https://www.youtube.com/c/Wolfkid')
-      .addField('Twitter', 'https://twitter.com/TheRealWolfkid')
-      .addField('GitHub', 'https://github.com/Wolfkid200444')
-      .addField('Fan Discord Server', 'http://bit.ly/WolfkidDiscord')
-      .addField('Website', 'In Progress')
-      .setColor('#FFFF00')
+      .setTitle('Github')
+      .setDescription('Information about the Bots GitHub')
+      .addField('YouTube', 'https://youtube.com/emeraldassasinplayz')
+      .addField('Website', 'https://minehub.de')
+      .addField('GitHub', 'https://github.com/StrikeDevelopment')
+      .addField('Discord', 'https://discord.gg/x3yyCqX')
+      .addField('Bot Host', 'https://glitch.com')
+      .setColor('#FFFFFF')
+      .setFooter('Request by ${message.author.tag} | Media Page 1/2')
       return message.channel.send(mediaembed);
     }
 
-    if (message.content === prefix + 'botinfo') {
+    if (message.content === prefix + 'info') {
 
       let botinfoembed = new Discord.RichEmbed()
-      .setTitle('BotInfo')
-      .setDescription('Here is were you can find About the Bot Info!')
-      .addField('Version', 'FWK-1.2 Unstable')
-      .setImage(bot.user.avatarURL)
-      .addField('Bot Created', bot.user.createdAt)
-      .addField('Bot Username', bot.user.username)
+      .setTitle('Information About the bot')
+      .setDescription('Bot information you may need to know')
+      .addField('Version', 'FMCDEK-1.2 stable')
+      .setImage('https://cdn.discordapp.com/attachments/436240644042653698/451177021423484929/thKKQYYMPK.jpg')
+      .addField('Bot Language', 'Discord.JS')
+      .addField('Bot Creator', 'ZarkysMC-YT#7314')
+      .setFooter('Bot Information | Page 1/2')
       return message.channel.send(botinfoembed);
     }
 
     if (message.content === prefix + 'changelog') {
       let changelogembed = new Discord.RichEmbed()
-      .setTitle('Changelog')
-      .setDescription('Check out Whats New on The Bot :D')
-      .addField('May 11 2018 News', 'Code Added')
-      .addField('May 14 2018 News Huge Update', 'Added Embeds To Commands And Alot')
-      .addField('May 18 2018 News', 'Added Help Command And Few Commands Working again')
-      .addField('May 27 2018 News', 'Added Footers to Commands and Avatar Command')
-      .setColor('#808000')
-      .setFooter(`Requested by ${message.author.tag}`)
+      .setTitle('Changlog')
+      .setDescription('[!] Recent Changelog On the BOT [!]')
+      .addField('May 25 2018 News', 'Ban Command Added')
+      .addField('May 26 2018 News Huge Update', 'Added Embeded Command Messages')
+      .addField('May 26 2018 News', 'Added Help Command Fixed BotHelp')
+      .addField('May 29 2018 News', 'Added Bot LevelUP System | Beta')
+      .addField('May 29 2018 News', 'Added Links To Media Command')
+      .addField('May 29 2018 News', 'Removed Old Crappy Welcome System')
+      .addField('May 29 2018 News', 'Updated Bot Info Command Message')
+      .setFooter('Auto Updated Thanks To Github | MightyVPS')
+      .setColor('#RANDOM')
       return message.channel.send(changelogembed);
     }
 
-    if (message.content === prefix + 'github') {
+    if (message.content === prefix + 'donate') {
       let githubembed = new Discord.RichEmbed()
-      .setTitle('Check out our GitHub')
-      .setURL('https://github.com/FutureDeveloperZ/')
-      .setDescription('Check out our GitHub to view all of our code or our repository.')
-      .setColor('#000000')
+      .setTitle('Donate To Keep Bot Up')
+      .setURL('https://paypal.me/caulden')
+      .setDescription('Donations Keep bot alive as bot is paid monthly through a VPS System')
+      .setColor('RANDOM')
       return message.channel.send(githubembed);
     }
 
-    if (message.content === prefix + 'dcinvite') {
+    if (message.content === prefix + 'announce') {
       let dcinviteembed = new Discord.RichEmbed()
-      .setTitle('Click Here to Our Discord Invite')
-      .setURL('http://bit.ly/FutureDevs')
-      .setDescription('Check out our Discord server to get news of FakeWolfkid and if you have issues with the bot or not working property you can report it on #report or on our GitHub')
+      .setTitle('Ooops Something went wrong')
+      .setDescription('OH NO! It Looks like you dont have Premium')
+      .addField('How Do I Get Premium', 'Get Premium by donating')
+      .addField('What is the donation link', 'Get it from typing the command -donate')
+      .addField('I Just Donated wheres my premium', 'Donating from credit or debit card purchases can take up to 2 days to proccess')
+      .setFooter('Donations Balance For May | $6.10')
+      .setColor('RANDOM')
       return message.channel.send(dcinviteembed);
     }
     
-   if (message.content === prefix + 'avatar') {
+   if (message.content === prefix + 'profile') {
      let avatarembed = new Discord.RichEmbed()
-     .setTitle('Avatar!')
-     .setColor('RANDOM')
+     .setTitle('Profile Center')
      .setImage(message.author.avatarURL + "?size=2048") 
-     .setFooter(`Requested by ${message.author.tag}`)
+     .setDescription('**Level** N/A')
+     .setDescription('**Awards** Discord Email Verified')
+     .setDescription('**Rank** | A Future Feature')
+     .setFooter('Profile Of ${message.author.tag}')
      return message.channel.send(avatarembed)
    }
    
